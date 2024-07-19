2351ccf20f064b5d2736e01dacabb18184191c0e
Mathias <email@example.com>
2024-07-19 09:53:04 +0000

    save

--- examples/struct_new_5.rs
+++ examples/struct_new_5.rs
@@ -6,6 +6,24 @@ struct Person {
     name: String,
     age: u8,
     is_student: bool,
+    
+}
+
+impl Person {
+
+   
+
+    // Define a constructor function `new` that takes `name`, `age` and
+    // `is_student` parameters 
+    fn new(name: String, age: u8, is_student: bool) -> Self {
+        // Create a new instance of `Person` struct and initialize its fields
+        // with the provided values 
+        Self {
+            name,        
+            age,        
+            is_student,
+        }
+    }
 }
 
 fn main() {
@@ -16,4 +34,8 @@ fn main() {
     };
 
     println!("person {:#?}", _person);
+
+    let _person_new = Person::new(String::from("John"), 27, true);
+
+    println!("person {:#?}", _person_new);
 }
---
+++ examples/yt_rust_pointers_1.rs
@@ -0,0 +1,24 @@
+// FROM HERE
+// https://www.youtube.com/watch?v=KYJ95TxEC18
+
+struct Node {
+    val: i32,
+    points_to: Option<Box<Node>>,
+}
+
+fn main() {
+    let n3 = Box::new(Node {
+        val: 3,
+        points_to: None,
+    });
+    let n2 = Node {
+        val: 2,
+        points_to: Some(n3),
+    };
+    let n1 = Node {
+        val: 1,
+        points_to: Some(n3),
+    };
+}
+
+
